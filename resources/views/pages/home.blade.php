c9ac079338961f27514c46a89fa0c85fd0f79041
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-03-14 15:12:42 +0100

    feat(all) /add-cards /functional

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,7 +5,33 @@
 @section('content')
 
 <main>
-    hello
+
+    <div class="container">
+
+    @foreach ( $movies as $item)
+    <div class="card">
+        <h2>{{$item['title']}}</h2>
+        <h4>Original Title: {{$item['original_title']}}</h4>
+        <div>
+            <b>Nationality:</b>
+            {{$item['nationality']}}
+        </div>
+        <div>
+            <b>Date:</b>
+            {{$item['date']}}
+        </div>
+        <div>
+            <b>Rating:</b>
+            {{$item['vote']}}
+        </div>
+    </div>
+    @endforeach
+
+
+
+    </div>
+
+
 </main>
 
 @endsection
